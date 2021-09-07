8955a19d9069c6b4706d093fb13c90408271ab6b
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-09-06 22:01:46 -0500

    webpack added

--- photo-app/src/main.ts
+++ photo-app/src/main.ts
@@ -1,4 +1,7 @@
-import { User, Album, Picture, PhotoOrientation } from './photo-app';
+import { Album } from "./album";
+import { PhotoOrientation } from "./photo-orientation";
+import { Picture } from "./picture";
+import { User } from "./user";
 
 const user = new User(1, 'angarita', 'David', true);
 const album = new Album(10, 'Platzi Pictures');
@@ -8,4 +11,7 @@ const picture = new Picture(1, 'TypeScript Course', '2021-09', PhotoOrientation.
 user.addAlbum(album);
 album.addPicture(picture);
 
+console.log('user', user);
+
+user.removeAlbum(album);
 console.log('user', user);
